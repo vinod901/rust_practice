4de4582834be3eee3b99cfe9c7cd90bfa718ffdc
vinod901 <yerrapureddyvinodreddy@gmail.com>
2021-06-28 13:07:29 +0530

    first commit in new_branch

--- src/integer_input.rs
+++ src/integer_input.rs
@@ -1,13 +1,12 @@
+use std::io;
 pub fn run() {
     let mut a = String::new();
     println!("Give some input :");
-    io::stdin()
-        .read_line(&mut a)
-        .expect("some error occured...!");
+    io::stdin().read_line(&mut a).unwrap();
     println!("input : {}", a);
     let trimmed = a.trim();
     match trimmed.parse::<u32>() {
-        Ok(i) => println!("your integer input: {}", i),
+        Ok(i) => println!("your integer is {}", i),
         Err(..) => println!("this was not an integer: {}", trimmed),
     }
 }
