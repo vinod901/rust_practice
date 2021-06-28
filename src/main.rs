59499f4de906dabc6fba322e70d359a2d9684bcf
vinod901 <yerrapureddyvinodreddy@gmail.com>
2021-06-28 13:01:02 +0530

    new programs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 // mod input;
-mod hashmap;
+// mod hashmap;
+mod tables;
+// mod integer_input;
 fn main() {
 	// input::run();
-	hashmap::run();
+	// hashmap::run();
+	tables::run();
+	// integer_input::run();
 }
