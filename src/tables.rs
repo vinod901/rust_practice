3737c65fdd250c7791f4210a646250fe62cdfbc1
vinod901 <yerrapureddyvinodreddy@gmail.com>
2021-07-02 22:42:14 +0530

    ready to merge this branch with the master branch

--- src/tables.rs
+++ src/tables.rs
@@ -1,18 +1,15 @@
-// use std::io;
+use std::io;
 pub fn run() {
-    // let mut num = String::new();
-    // println!("Give some input :");
-    // io::stdin().read_line(&mut num).unwrap();
-    // let trimmed = num.trim();
-    // match trimmed.parse::<u32>() {
-    //     Ok(i) => {
-    //         for digit in 1..11 {
-    //             println!("{} * {} = {}", i, digit, i * digit);
-    //         }
-    //     }
-    //     Err(..) => println!("this was not an integer: {}", trimmed),
-    // }
-    for i in 1..11 {
-        println!("{} * {} = {}", 5, i, 5 * i);
+    let mut num = String::new();
+    println!("Give some input :");
+    io::stdin().read_line(&mut num).unwrap();
+    let trimmed = num.trim();
+    match trimmed.parse::<u32>() {
+        Ok(i) => {
+            for digit in 1..11 {
+                println!("{} * {} = {}", i, digit, i * digit);
+            }
+        }
+        Err(..) => println!("this was not an integer: {}", trimmed),
     }
 }
