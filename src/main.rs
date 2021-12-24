b60d1f50759f5ae028da20f098cf760fd00adf16
charan r <charanr@charans-MacBook-Pro.local>
2021-12-24 16:47:14 +0530

    copied some  code form

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 #[macro_use]
 extern crate rocket;
 mod files;
-use files::routes::{get_query, hello, read_csv, send_data};
+use files::{
+    routes::{get_query, hello, read_csv, send_data},
+    sql::sql_select,
+};
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![hello, read_csv, get_query, send_data])
+    rocket::build().mount(
+        "/",
+        routes![hello, read_csv, get_query, send_data, sql_select],
+    )
 }
